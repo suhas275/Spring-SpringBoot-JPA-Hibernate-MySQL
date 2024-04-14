0baf177546b0454d0d78686a2b6752fb7bbdb6f5
Suhas Gowda <67008179+suhas275@users.noreply.github.com>
2024-04-14 15:18:04 +0000

    initial commit

---
+++ Project_1/src/main/java/com/edigest/Project_1/HelloController.java
@@ -0,0 +1,12 @@
+package com.edigest.Project_1;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @RequestMapping("/hello")
+    public String hello(){
+        return "hello world";
+    }
+}

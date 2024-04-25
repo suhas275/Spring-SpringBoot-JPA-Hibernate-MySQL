3f2a29f8b74812c8f712c5d4a4b8e4d68b88fc92
Suhas Gowda <67008179+suhas275@users.noreply.github.com>
2024-04-25 06:06:11 +0000

    CRUD update

---
+++ CRUDdemo/src/test/java/com/example/CRUDdemo/CruDdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CRUDdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CruDdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
